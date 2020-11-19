d4e3d027f20c53dea197eb498e69355eae94e968
JD Gardner <jdgardner@aptuscapitaladvisors.com>
2020-11-19 16:14:56 -0600

    setting up to deploy

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const POKEDEX = require('./pokedex.json')
 
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -70,6 +71,20 @@ app.get('/types', function handleGetTypes(req, res) {
     res.json(response)
   })
 
-app.listen(8000, () => {
-  console.log("server listening on 8000");
-});
+  app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+  
+  const PORT = process.env.PORT || 8000
+
+  const PORT = process.env.PORT || 8000
+
+  app.listen(PORT, () => {
+  
+  });
